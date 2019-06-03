840bfa81f881487b5f1e90c0907bafbbb6ec63c2
josephHai <junyan0803@gmail.com>
2019-06-03 22:33:35 +0800

    post模块初始化

---
+++ post/src/main/java/com/myhomie/module/post/main/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.myhomie.module.post.main;
+
+import android.os.Bundle;
+
+import com.myhomie.module.common.base.BaseActivity;
+import com.myhomie.module.post.R;
+
+public class LauncherActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
