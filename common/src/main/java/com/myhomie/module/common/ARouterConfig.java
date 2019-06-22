9b482c9859a6be9e050f7e4f22388ba12f4a9e9b
josephHai <junyan0803@gmail.com>
2019-06-22 14:51:22 +0800

    ARouter一级路由不可以是main

--- common/src/main/java/com/myhomie/module/common/ARouterConfig.java
+++ common/src/main/java/com/myhomie/module/common/ARouterConfig.java
@@ -1,7 +1,7 @@
 package com.myhomie.module.common;
 
 public class ARouterConfig {
-    public static final String MAIN_ACTIVITY = "/main/main";
+    public static final String MAIN_ACTIVITY = "/app/main";
     public static final String POST_DETAIL_ACTIVITY = "/post/detail";
     public static final String LOGIN_ACTIVITY = "/login/main";
 }
